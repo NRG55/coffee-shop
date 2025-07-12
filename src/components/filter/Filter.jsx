98f2bf747576311ec235bbe57e3b4e3cb124ad35
NRG55 <maloj_1980@yahoo.fr>
2025-07-13 00:56:46 +0100

    Update(Filter.js): a categories section is added (css module is updated)

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,27 +2,40 @@ import styles from './Filter.module.css';
 import data from '../../data/products.json';
 
 const Filter = () => {
+    const categories = ["All", "Robusta", "Arabica", "Liberica", "Excelsa"];
     const setBrands = new Set(data.map((product) => product.brand));
     const brands = [...setBrands];
 
     return (
         <>
             <div className={styles.filterContainer}> 
-                <h3>Brands</h3>
-                <div className={styles.inputsContainer}>           
-                    {brands.map((brand, index) =>
-                        <div key={`${brand + index}`} className={styles.inputWrapper}>                            
-                            <label htmlFor={brand}>{brand}
-                                <input
-                                    type="checkbox"
-                                    id={brand}
-                                    name={brand} 
-                                />
-                                <span class={styles.checkmark}></span>
-                            </label>
-                        </div>
-                    )}
-                </div>            
+                <div className={styles.section}>
+                    <h3>CATEGORY</h3>
+                    <div className={styles.buttonsContainer}>           
+                        {categories.map((category) =>
+                            <div key={category}>                            
+                                <button >{category}</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className={styles.section}>           
+                    <h3>BRANDS</h3>
+                    <div className={styles.inputsContainer}>           
+                        {brands.map((brand) =>
+                            <div key={brand} className={styles.inputWrapper}>                            
+                                <label htmlFor={brand}>{brand}
+                                    <input
+                                        type="checkbox"
+                                        id={brand}
+                                        name={brand} 
+                                    />
+                                    <span class={styles.checkmark}></span>
+                                </label>
+                            </div>
+                        )}
+                    </div>
+                </div>             
             </div>            
         </>
     );
